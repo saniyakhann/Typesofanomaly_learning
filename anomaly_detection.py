a15e7eeaf9539fc55c18367ca435c61c25ab0f46
saniyakhann <161816857+saniyakhann@users.noreply.github.com>
2025-08-28 09:58:02 +0530

    anomaly_detection.py

--- anomaly_detection.py
+++ anomaly_detection.py
@@ -4,24 +4,24 @@ from adtk.data import validate_series
 from adtk.visualization import plot
 from adtk.detector import ThresholdAD, QuantileAD
 
-# Load the data
+#load the data
 data = pd.read_csv("data-20240706.csv")
 print(data.head())
 
-# Filter data and set index
+#filter data and set index
 data = data.loc[data["logical_site_id"] == 6420]
 data["period_start_time"] = pd.to_datetime(data["period_start_time"])
 data = data.set_index("period_start_time")
 data = data["rrc_accessibility"]
 print(data.head())
 
-# Validate series
+#validate series
 data = validate_series(data)
 
-# Quantile-based anomaly detection
+#quantile-based anomaly detection
 quantile_detector = QuantileAD(low=0.01, high=0.99)
 anomalies = quantile_detector.fit_detect(data)
 
-# Plot the results
+#plot the results
 plot(data, anomaly=anomalies, anomaly_color="red", anomaly_tag="marker")
 plt.show()
